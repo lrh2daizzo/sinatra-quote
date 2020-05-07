1692c6a66a84cbe92fc73489f4bdc269894035ca
lrh2daizzo <herrera.lrh@gmail.com>
2020-05-07 18:37:56 -0500

    setup config.ru & app controller

--- db/migrate/20200506231720_create_quotes.rb
+++ db/migrate/20200506231720_create_quotes.rb
@@ -1,7 +1,10 @@
 class CreateQuotes < ActiveRecord::Migration[6.0]
   def change
     create_table :quotes do |t|
-      t.text :
+      t.text :content
+      t.integer :user_id
+
+      t.timestamps null: false
     end
   end
 end
