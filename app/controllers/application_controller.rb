5ba2069b993ca8d46eee741fc4842108f3c78c97
lrh2daizzo <herrera.lrh@gmail.com>
2020-05-07 20:57:47 -0500

    modified app cntrlr,  config and rakefile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
+    configure do
+        set :public_folder, 'public'
+        set :views, 'app/views'
+        enable :sessions
+        set :session_secret, "sinatra_q"
+    end
+
+    get '/' do
+        "Hello World"
+    end
 end
